d9659d5020d358ee6d2fbe39639e183d10be7895
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2020-08-22 15:20:33 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -6,27 +6,34 @@ import './App.css';
 import Header from './components/Header';
 
 function App() {
-    const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+    const [projects, setProjects] = useState([]);
 
     useEffect(() => {
         api.get('projects').then(response => {
-            console.log(response);
-        })
+            setProjects(response.data);
+        });
     }, [])
 
-    function handleAddProject() {
+    async function handleAddProject() {
         //project.push(`Novo projeto ${Date.now()}`)
 
-        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+        // setProjects([...projects, `Novo projeto ${Date.now()}`]);
 
-        console.log(projects);
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Marco Pinho"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
     }
 
     return (
         <>
             <Header title="Projects" />
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
             </ul>
 
             <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
